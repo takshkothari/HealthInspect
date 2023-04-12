7581b7820964e22638cf76669788a3d317564590
Taksh Kothari <96124613+takshkothari@users.noreply.github.com>
2023-04-12 18:29:49 +0530

    app init

---
+++ app.py
@@ -0,0 +1,76 @@
+from flask import Flask, render_template, request
+
+import cv2
+import imutils
+from matplotlib import pyplot as plt
+import numpy as np
+import seaborn as sns
+import os
+import pathlib
+
+import base64
+from google.cloud import aiplatform
+from google.cloud.aiplatform.gapic.schema import predict
+
+
+app=Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def hello_world():
+    print("hello")
+    return render_template('index.html')
+
+@app.route('/',methods=['POST'])
+def predict_image_classification_sample(
+    project="738477523269",
+    endpoint_id="5132357550737457152",
+    location="us-central1",
+    filename="/Users/joshvirnarula/gfg_hackathon/misc/image.jpeg",
+    api_endpoint= "us-central1-aiplatform.googleapis.com"):
+    # The AI Platform services require regional API endpoints.
+    client_options = {"api_endpoint": api_endpoint}
+    # Initialize client that will be used to create and send requests.
+    # This client only needs to be created once, and can be reused for multiple requests.
+    print("connecting client..")
+    client = aiplatform.gapic.PredictionServiceClient(client_options=client_options)
+    print("connected")
+    with open(filename, "rb") as f:
+        file_content = f.read()
+
+    # The format of each instance should conform to the deployed model's prediction input schema.
+    encoded_content = base64.b64encode(file_content).decode("utf-8")
+    instance = predict.instance.ImageClassificationPredictionInstance(
+        content=encoded_content,
+    ).to_value()
+    instances = [instance]
+    # See gs://google-cloud-aiplatform/schema/predict/params/image_classification_1.0.0.yaml for the format of the parameters.
+    parameters = predict.params.ImageClassificationPredictionParams(
+        confidence_threshold=0.1, max_predictions=5,
+    ).to_value()
+    endpoint = client.endpoint_path(
+        project=project, location=location, endpoint=endpoint_id
+    )
+    response = client.predict(
+        endpoint=endpoint, instances=instances, parameters=parameters
+    )
+    print("response")
+    print(" deployed_model_id:", response.deployed_model_id)
+    # See gs://google-cloud-aiplatform/schema/predict/prediction/image_classification_1.0.0.yaml for the format of the predictions.
+    predictions = response.predictions
+    for prediction in predictions:
+        print(" prediction:", dict(prediction))
+print("starting..")
+
+# def predict():
+#     imagefile=request.files['imagefile']
+#     image_path="./images/" + imagefile.filename
+#     imagefile.save(image_path)
+
+
+#     print(image_path)
+#     image = cv2.imread(image_path)
+#     image=cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+
+
+if __name__=='__main__':
+    app.run(port=3000,debug=True)
